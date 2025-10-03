0cdc64f483e7cfcb0d0345618a404dc8291d2278
DanSamedov <danasamedov@gmail.com>
2025-10-03 23:23:26 +0000

    Add page counter and generator to get all job ids from all the pages available

--- app/tasks/scrape.py
+++ app/tasks/scrape.py
@@ -34,6 +34,10 @@ class Scrape(webdriver.Chrome):
         super().__init__(service=svc, options=opts)
 
 
+    def __enter__(self):
+        return self
+
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.teardown:
             self.quit()
@@ -83,15 +87,16 @@ class Scrape(webdriver.Chrome):
             }
 
 
-    def get_job_ids(self, page_url: str) -> List[int]:
-        self.open(page_url)
-        job_ids = []        
+    def get_job_ids(self, url: str) -> List[int]:
+        self.open(url)
+
         self.wait.until(
             EC.presence_of_all_elements_located((By.CSS_SELECTOR, "ul.list-jobs li[id^='job-item-']"))
         )
 
         items = self.find_elements(By.CSS_SELECTOR, "ul.list-jobs li[id^='job-item-']")
 
+        job_ids = []
         for item in items:
             li_id = item.get_attribute("id")
             job_id = li_id.replace("job-item-", "")
@@ -100,6 +105,35 @@ class Scrape(webdriver.Chrome):
         return job_ids
 
 
+    def get_total_pages(self, url: str) -> int:
+        self.open(url)
+
+        self.wait.until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "ul.pagination_with_numbers"))
+        )
+        page_items = self.find_elements(By.CSS_SELECTOR, "li.page-item")
+
+        total_pages = 1
+        for item in page_items:
+            try:
+                number = int(item.text.strip())
+                if number > total_pages:
+                    total_pages = number
+            except ValueError:
+                continue
+
+        return total_pages
+
+
+    def iter_job_ids(self, url: str):
+        total_pages = self.get_total_pages(url)
+
+        for page in range(1, total_pages + 1):
+            paged_url = f"{url}my/dashboard/?page={page}"
+            job_ids = self.get_job_ids(paged_url)
+            yield from job_ids
+
+
 with Scrape() as bot:
-    job_ids = bot.get_job_ids("https://djinni.co/")
-    print(job_ids)
+    for job_id in bot.iter_job_ids("https://djinni.co/"):
+        print(job_id)
