4217886b5763ad0584aeae414bf402de2eb31356
DanSamedov <danasamedov@gmail.com>
2025-12-12 22:33:18 +0100

    Create pydantic schemas for the Job models

---
+++ app/schemas/job.py
@@ -0,0 +1,68 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List, Optional
+from app.core.enums import JobStatus, JobSource, FormFieldType, APIStatus, APIError
+
+
+class JobStub(BaseModel):
+    id: int
+    external_id: int
+    source: JobSource
+    status: JobStatus
+    found_at: datetime
+
+
+class JobStubCreate(BaseModel):
+    external_id: int
+    source: JobSource = JobSource.DJINNI
+
+
+class JobStubUpdate(BaseModel):
+    status: JobStatus
+
+
+class JobDetails(BaseModel):
+    id: int
+    title: str
+    company: str
+    description: str
+    link: str
+    scraped_at: Optional[datetime]
+
+
+class JobDetailsCreate(BaseModel):
+    external_id: int
+    title: str
+    company: str
+    description: str
+    link: str
+
+
+class AnswerOption(BaseModel):
+    text: str
+    value: str
+
+
+class JobFormField(BaseModel):
+    id: int
+    external_field_id: str
+    question: str
+    answer_type: FormFieldType
+    answer_options: Optional[List[AnswerOption]]
+    answer: Optional[str]
+    scraped_at: Optional[datetime]
+    sent_at: Optional[datetime]
+
+
+class JobFormFieldCreate(BaseModel):
+    external_field_id: str
+    question: str
+    answer_type: FormFieldType
+    answer_options: Optional[List[AnswerOption]]
+
+
+class JobResponse(BaseModel):
+    status: APIStatus
+    external_id: Optional[int] = None
+    id: Optional[int] = None
+    error: Optional[APIError] = None
